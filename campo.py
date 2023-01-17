5865499fff3f1555073c19c3eb91be9338c165da
Izak A <91832540+Izak76@users.noreply.github.com>
2023-01-17 15:02:45 -0300

    Adição de botões para configurações e novo jogo

--- campo.py
+++ campo.py
@@ -2,8 +2,7 @@ import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
 
 from utils.colors import *
-from sprites import Dial, Minefield
-import pygame.draw as draw
+from sprites import *
 import pygame
 
 
@@ -26,7 +25,15 @@ dial_x = minas_pos[0]+minas.get_width()+(1-p)*sobra/2
 dial_wh = (sobra*p, sobra*0.3)
 tempo = Dial("src/chronometer.png", pygame.Rect(dial_x, minas_pos[1]+height//60, *dial_wh), rp)
 mcount = Dial("src/bomb.png", pygame.Rect(dial_x, minas_pos[1]+tempo.rect.height+height//12, *dial_wh), rp)
-sprites = pygame.sprite.Group(tempo, mcount, mf)
+
+rb1 = pygame.Rect(0, 0, *(sobra*0.25,)*2)
+rb1.x = minas_pos[0]+minas.get_width()+sobra*0.1
+rb1.y = minas_pos[1]+minas.get_height()-rb1.h
+rb2 = pygame.Rect(rb1.right+sobra*0.1, rb1.y, sobra*0.45, rb1.h)
+b1 = Button(rb1, image="src/gear.png")
+b2 = Button(rb2, label="Novo jogo")
+sprites1 = pygame.sprite.Group(tempo, mcount, mf)
+sprites2 = pygame.sprite.Group(mf, b1, b2)
 
 while True:
     for evt in pygame.event.get():
@@ -43,6 +50,7 @@ while True:
 
     screen.fill(BACKGROUND_COLOR)
 
-    mf.update()
-    sprites.draw(screen)
+    sprites1.draw(screen)
+    sprites2.update()
+    sprites2.draw(screen)
     pygame.display.flip()
