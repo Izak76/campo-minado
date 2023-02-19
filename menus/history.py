e4cca6929ad110273ce34645a6285e05b8ea1a44
Izak A <91832540+Izak76@users.noreply.github.com>
2023-02-18 21:39:01 -0300

    Implementação da leitura de arquivo de histórico

--- menus/history.py
+++ menus/history.py
@@ -1,34 +1,39 @@
-import tkinter as tk
+from . import MenuBase
+from utils.persistent import History
+from .statistics import StatisticsMenu
 import tkinter.ttk as ttk
-from menus import MenuBase
+import tkinter as tk
+import time
 
 
-class History(MenuBase):
-    def __init__(self, master:tk.Tk|tk.Toplevel|None=None):
+class HistoryMenu(MenuBase):
+    def __init__(self, hist:History, master:tk.Tk|tk.Toplevel|None=None):
         self.width = 560
         self.height = 300
         self.title = "Histórico"
-        super().__init__(master)
-        self.mainwindow.resizable(1, 1)
+        super().__init__(master, True)
+        self.hist = hist
 
-        stats = ttk.Button(self.mframe, default="active", text='Estatísticas', width=15)
+        stats = ttk.Button(self.mframe, default="active", text='Estatísticas', width=15, command=self.__open_statistics)
         stats.pack(anchor="w", padx=5, pady=10, side="top")
 
         frame2 = ttk.Frame(self.mframe, height=200, width=200)
-        cols = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6']
+        cols = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7']
         self.history_table = ttk.Treeview(frame2, selectmode="extended", show="headings", columns=cols, displaycolumns=cols)
-        self.history_table.column("col1", anchor="w", stretch=True, width=100, minwidth=20)
-        self.history_table.column("col2", anchor="w", stretch=True, width=125, minwidth=20)
-        self.history_table.column("col3", anchor="w", stretch=True, width=75, minwidth=20)
-        self.history_table.column("col4", anchor="w", stretch=True, width=75, minwidth=20)
-        self.history_table.column("col5", anchor="w", stretch=True, width=75, minwidth=20)
-        self.history_table.column("col6", anchor="w", stretch=True, width=75, minwidth=20)
+        self.history_table.column("col1", anchor="w", stretch=True, width=70, minwidth=20)
+        self.history_table.column("col2", anchor="w", stretch=True, width=115, minwidth=20)
+        self.history_table.column("col3", anchor="center", stretch=True, width=60, minwidth=20)
+        self.history_table.column("col4", anchor="w", stretch=True, width=85, minwidth=20)
+        self.history_table.column("col5", anchor="center", stretch=True, width=75, minwidth=20)
+        self.history_table.column("col6", anchor="center", stretch=True, width=75, minwidth=20)
+        self.history_table.column("col7", anchor="center", stretch=True, width=50, minwidth=20)
         self.history_table.heading("col1", anchor="w", text='Pontuação')
         self.history_table.heading("col2", anchor="w", text='Dia e Horário')
-        self.history_table.heading("col3", anchor="w", text='Tabuleiro')
-        self.history_table.heading("col4", anchor="w", text='Nº de minas')
-        self.history_table.heading("col5", anchor="w", text='Minas marcadas')
-        self.history_table.heading("col6", anchor="w", text='Vitória?')
+        self.history_table.heading("col3", anchor="center", text='Duração')
+        self.history_table.heading("col4", anchor="w", text='Dificuldade')
+        self.history_table.heading("col5", anchor="center", text='Nº de minas')
+        self.history_table.heading("col6", anchor="center", text='Minas marcadas')
+        self.history_table.heading("col7", anchor="center", text='Vitória?')
         self.history_table.pack(expand=True, fill="both", side="left")
         
         scroll_x = ttk.Scrollbar(self.mframe, orient="horizontal", command=self.history_table.xview)
@@ -40,10 +45,13 @@ class History(MenuBase):
         scroll_y.pack(expand=False, fill="y", side="left")
         self.mframe.pack(expand=True, fill="both", side="top")
 
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = History(root)
-    app.run()
-
-
+        for items in hist:
+            items[1] = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(items[1]))
+            items[3] = ("Fácil", "Médio", "Difícil", "Personalizado")[items[3]]
+            items[6] = "Sim" if items[6] else "Não"
+            self.history_table.insert('', 'end', values=items)
+    
+    def __open_statistics(self):
+        top = tk.Toplevel(self.mainwindow)
+        st = StatisticsMenu(self.hist, top)
+        st.run()
--- menus/statistics.py
+++ menus/statistics.py
@@ -1,45 +1,78 @@
-import tkinter as tk
+from utils.persistent import History
+from . import MenuBase
 import tkinter.ttk as ttk
-from menus import MenuBase
+import tkinter as tk
 
 
-class Statistics(MenuBase):
-    def __init__(self, master:tk.Tk|tk.Toplevel|None=None):
+class StatisticsMenu(MenuBase):
+    def __init__(self, hist:History, master:tk.Tk|tk.Toplevel|None=None):
         self.width = 400
-        self.height = 180
+        self.height = 200
         self.title = "Estatísticas"
         super().__init__(master)
 
-        self.list_diffs = tk.Listbox(self.mframe, borderwidth=2, height=6, relief="groove", width=25, highlightthickness=0, activestyle="none")
-        for d in ("Geral", "Fácil", "Médio", "Difícil", "Personalizado"):
-            self.list_diffs.insert('end', d)
+        self.list_diffs = tk.Listbox(self.mframe, borderwidth=2, height=7, relief="groove", width=25, highlightthickness=0, selectmode="single", activestyle="none")
+        self.list_diffs.insert('end', "Geral", "Fácil", "Médio", "Difícil", "Personalizado")
+        self.list_diffs.bind("<<ListboxSelect>>", self.set_statistic)
         self.list_diffs.select_set(0)
 
         ok = ttk.Button(self.mframe, default="active", text='OK', width=14, command=self.mainwindow.destroy)
-        frame1 = ttk.Frame(self.mframe, height=100, width=200)
+        frame1 = ttk.Frame(self.mframe, height=125, width=200)
         frame1.grid_propagate(False)
 
         ttk.Label(frame1, text='Partidas jogadas:').grid(column=0, pady=3, row=0, sticky="w")
         ttk.Label(frame1, text='Vitórias:').grid(column=0, pady=3, row=1, sticky="w")
-        ttk.Label(frame1, text='Derrotas:').grid(column=0, pady=3, row=2, sticky="w")
-        ttk.Label(frame1, text='Percentual de vitórias:').grid(column=0, ipadx=10, pady=3, row=3, sticky="w")
+        ttk.Label(frame1, text='Percentual de vitórias:').grid(column=0, ipadx=10, pady=3, row=2, sticky="w")
+        ttk.Label(frame1, text='Maior pontuação:').grid(column=0, pady=3, row=3, sticky="w")
+        ttk.Label(frame1, text='Menor tempo:').grid(column=0, pady=3, row=4, sticky="w")
 
         self.pj = ttk.Label(frame1)
         self.vit = ttk.Label(frame1)
-        self.der = ttk.Label(frame1)
         self.pv = ttk.Label(frame1)
+        self.mp = ttk.Label(frame1)
+        self.mt = ttk.Label(frame1)
 
         self.pj.grid(column=1, row=0, sticky="w")
         self.vit.grid(column=1, row=1, sticky="w")
-        self.der.grid(column=1, row=2, sticky="w")
-        self.pv.grid(column=1, row=3, sticky="w")
+        self.pv.grid(column=1, row=2, sticky="w")
+        self.mp.grid(column=1, row=3, sticky="w")
+        self.mt.grid(column=1, row=4, sticky="w")
         
         self.list_diffs.pack(anchor="n", padx=15, pady=15, side="left")
-        frame1.pack(pady=15, side="top")
+        frame1.pack(pady=12, side="top")
         ok.pack(anchor="e", padx=20, pady=10, side="top")
 
+        # stats[dificuldade][part. jog.|vits.|maior p.|menor t.]
+        self.__stats = tuple(([0]*4 for _ in range(4)))
+        for h in hist:
+            points, time, diff, vit = h[0], h[2], h[3], h[6]
+            d = self.__stats[diff]
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = Statistics(root)
-    app.run()
+            d[0] += 1
+            d[1] += vit
+
+            if points > d[2]:
+                d[2] = points
+            
+            if time < d[3] or not d[3]:
+                d[3] = time
+        
+        self.set_statistic()
+    
+    def set_statistic(self, evt:tk.Event|None=None):
+        index ,= self.list_diffs.curselection()
+
+        if not index:
+            get_items = lambda i: (x[i] for x in self.__stats)
+
+            self.pj['text'] = sum(get_items(0))
+            self.vit['text'] = sum(get_items(1))
+            self.mp['text'] = max(get_items(2))
+            self.mt['text'] = min(get_items(3))
+        
+        else:
+            for idx, label in enumerate(map(self.__getattribute__, 'pj vit mp mt'.split())):
+                label['text'] = self.__stats[index-1][idx]
+        
+        self.pv['text'] = f"{self.vit['text']/self.pj['text']*100:.1f}%"
+            
